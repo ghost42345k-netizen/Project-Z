d969bc6a015d74f47a4274236816137f73519853
Oblivion-X <ghost42345k@gmail.com>
2026-09-03 15:51:14 -0300

    feat: Adicionar bot Discord

---
+++ src/bot/bot.js
@@ -0,0 +1,51 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const config = require('../config/config');
+const fs = require('fs');
+const path = require('path');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
+
+client.commands = new Collection();
+
+// Carregar eventos
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+// Carregar comandos
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  }
+}
+
+function startBot() {
+  if (!config.discord.token) {
+    console.error('[GANANCIA] ❌ DISCORD_TOKEN não configurado no .env');
+    process.exit(1);
+  }
+  client.login(config.discord.token);
+}
+
+module.exports = { client, startBot };
